8fcb33559a8dccfed907a994f703752b59fa751b
imran@techcity.ventures <imran@techcity.ventures>
2020-08-18 01:20:00 +0100

    heroku test2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()) 
 app.use(cors())
