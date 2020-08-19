a71c6b76786768537bb6ae1c6be7bfdbd77ef690
imran@techcity.ventures <imran@techcity.ventures>
2020-08-19 06:33:08 +0100

    added build folder heroku test16

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.use(cors())
 app.options('*', cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-if (process.env.NODE_ENV === 'production') {
-  
-  app.use(express.static('client/build'));
+
+
+  app.use(express.static('build'));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname,'build', 'index.html'));
  });
-}
+
 
 
 
