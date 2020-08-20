48cf269555adfb5eac3c1eec51160d7f6153ec33
imran@techcity.ventures <imran@techcity.ventures>
2020-08-20 02:03:20 +0100

    addded imran website

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const path = require('path');
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
-    // res.setHeader('Access-Control-Allow-Origin', 'https://imraann0.co.uk')
+    res.setHeader('Access-Control-Allow-Origin', 'https://imraann0.co.uk')
 
 
   // Request methods you wish to allow
